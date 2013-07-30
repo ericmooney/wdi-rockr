68b2c0e15df21f879ca75d159954fa3b21a01596
Sumeet Jain <sumeet@sumeetjain.com>
2013-07-30 10:30:52 -0700

    Homepage specs

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe "Homepage" do
+  it "displays the New Venue form via AJAX" do
+    visit root_path
+
+    click_link "New Venue"
+
+    expect(page).to have_css('form#new_venue')
+  end
+
+  it "displays the New Concert form via AJAX" do
+    visit root_path
+
+    click_link "New Concert"
+
+    expect(page).to have_css('form#new_concert')
+  end
+end
