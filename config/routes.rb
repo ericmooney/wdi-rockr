fff308e4550040e97c53a255fb12211f9922ead1
Eric Mooney <eric.c.mooney@gmail.com>
2013-08-01 10:23:27 -0700

    Add current user method and additional view logic in nav

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ WdiRockr::Application.routes.draw do
 
   get "auth/twitter/callback" => 'sessions#create'
 
+  get 'signout' => 'sessions#destroy', :as => 'signout'
+
   resources :venues, :only => [:new, :create]
   resources :concerts, :only => [:new, :create]
 
